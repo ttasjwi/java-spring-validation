2c32e35a7dde0e524489e9fca79b25cbac606e88
ttasjwi <ttasjwi920@gmail.com>
2022-03-31 21:34:31 +0900

    V4 - 컨트롤러에서 Form 전송객체 분리 적용
    - Item에서 BeanValidation 로직 제거
    - 컨트롤러에서 Form 전송객체로 바인딩하도록 변경

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,32 +1,28 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
-@Data
 //@ScriptAssert(
 //        lang = "javascript",
 //        script = "_this.price * _this.quantity >= 10000",
 //        message = "총합이 10000원이 넘도록 입력해주세요."
 //)
+@Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)  // 수정 요구사항 : update 시에는 id 값 존재여부를 확인해야한다.
+//    @NotNull(groups = UpdateCheck.class)  // 수정 요구사항 : update 시에는 id 값 존재여부를 확인해야한다.
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1_000, max = 1_000_000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1_000, max = 1_000_000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class}) // 수정 시에는 quantity 값에 제약조건을 주지 않는다.
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class}) // 수정 시에는 quantity 값에 제약조건을 주지 않는다.
     private Integer quantity;
 
     public Item() {
